201aa51ca80c1d0cebd9f2708345e061c6da540b
Mbasa Dunjwa <mbdunjwcpt024@student.wethinkcode.co.za>
2026-02-25 17:10:12 +0200

    Map practice to isolate data into an array

--- frontend/app.js
+++ frontend/app.js
@@ -3,6 +3,13 @@ async function fetchTopScorers() {
         const response = await fetch("http://127.0.0.1:5000/api/stats/top-scorers");
         const data = await response.json();
         console.log("Data received:", data)
+
+        const names = data.map(player => player.fullName)
+        const goals = data.map(player => player.totalGoals_value)
+
+        console.log("Names:", names)
+        console.log("Goals:", goals)
+        
         return data;
     } catch (error) {
         console.error("Failed to fetch:", error);
